72b8ac6fb62b7934618196377b76d28d96cdea57
Adrian <118555815+Hapm1997@users.noreply.github.com>
2024-01-07 01:09:48 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Telefono t1 = new Telefono("4dfgs33", "Samsung");
+            Console.WriteLine("ingrese el nnumero:");
+            t1.NumTel = Console.ReadLine();
+            Console.WriteLine("Ingrese el codigp de operador:");
+            t1.CodOper = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(t1.Marca);
+            Console.WriteLine(t1.Modelo);
+            Console.WriteLine(t1.NumTel);
+            Console.WriteLine(t1.CodOper);
+            Console.WriteLine(t1.llamar());
+            Console.WriteLine("Ingrese el contacto");
+            string contacto = Console.ReadLine();
+
+            Console.WriteLine(t1.llamar(contacto));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Telefono.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio_1
+{
+    internal class Telefono
+    {
+        private string modelo;
+        private string marca;
+        private string numTel;
+        private int codOper;
+
+        public Telefono( string modelo , string marca)
+        {
+            this.modelo = modelo;
+            this.marca = marca;
+        }
+
+        public string Modelo
+        {
+            get { return modelo; }
+        }
+
+        public string Marca
+        {
+            get { return marca; }
+        }
+
+        public string NumTel { get; set; }
+
+        public int CodOper
+        {
+            get { return codOper; }
+            set
+            {
+                if (value > 0 && value < 4)
+                {
+                    this.codOper= value;
+                }
+                else
+                {
+                    this.codOper= 0;
+                }
+            }
+        }
+
+        //METODO
+        public string llamar()
+        {
+            return "Realizando llamada";
+        }
+        public string llamar( string contacto)
+        {
+            return "Llamando a " + contacto;
+        }
+    }
+}
